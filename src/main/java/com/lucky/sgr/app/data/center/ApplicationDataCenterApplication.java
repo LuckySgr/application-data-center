fa0ce7aea17c0f1b6ef1e258ac5c928a06f20cae
谢小龙 <742875110@qq.com>
2023-02-14 14:42:29 +0800

    Initial commit

---
+++ src/main/java/com/lucky/sgr/app/data/center/ApplicationDataCenterApplication.java
@@ -0,0 +1,13 @@
+package com.lucky.sgr.app.data.center;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationDataCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationDataCenterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lucky/sgr/app/data/center/ApplicationDataCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucky.sgr.app.data.center;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationDataCenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
